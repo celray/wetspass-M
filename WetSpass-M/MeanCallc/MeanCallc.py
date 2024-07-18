0e97056ebde44adb00291b223e3d3112acb2e739
Celray James CHAWANDA <celray.chawanda@outlook.com>
2024-07-18 15:32:44 -0400

    Initial Commit

---
+++ WetSpass-M/MeanCallc/MeanCallc.py
@@ -0,0 +1,237 @@
+﻿# ++++++++++++++++++++++++++++++++++++++++++ headers +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+import clr
+import sys
+from h2pl import *
+clr.AddReference('System')
+clr.AddReference('System.Windows.Forms')
+clr.AddReference('System.Drawing')
+from System.Windows.Forms import *
+from System.Drawing import *
+from System import *
+from h2pl.macro import *
+from System import DateTime
+print    """ A program  for the calculation of Mean for WetSpass project
+'Author Khodayar Abdollahi   <kabdolla@vub.ac.be>,
+Dept. of Hydrology and Hydraulic Engineering, Vrije Universiteit Brussel
+(c) 2011
+"""
+
+class MeanCalc():
+    def __init__(self):
+       pass
+    class Model(Form):
+#       | <=required indentation 
+         
+        def __init__(self):
+         #A function to load GUI
+          self.loadpage()          
+  
+        def DoSomething(self, sender, e):
+            try:
+                 if len(self.Tboxworkingdir.Text)<1:
+                      raise Exception("What folder? Working directory is empty." ) 
+                
+                 currenstep=int(self.Tboxstarttimestep.Text)
+                 finaltimstep=int(self.Tboxendtimestep.Text)
+                 self.gotError=False
+                 curPeriodNo=0
+                 #curPeriodNo=self.TxtFirstStepNo.Text           
+           
+                 while currenstep < finaltimstep+1 and self.gotError==False:               
+                
+                     self.Sumup(currenstep) 
+                     print "Processing started for time step "+str(currenstep)
+               
+                     currenstep=currenstep+1
+            
+                 print "Stat has been run successfully!"
+                 MessageBox.Show("statistics has been run successfully!"+'\n' +"To see the results check  output  "+'\n' + self.Tboxworkingdir.Text+"\\"+self.TxtSimulations.Text)
+            except Exception, exceptionObject: 
+                  MessageBox.Show(exceptionObject.Message.ToString() +'\n' +'Line ' + str(sys.exc_info()[-1].tb_lineno) + '\n'+ "failed to finish the job")
+                  return
+            finally:
+                 pass    
+            
+            
+        def EditMyself(self, sender, e):
+                showeditor("")
+        def ButworkdirClick(self, sender, e):
+                self.folderBrowserDialog1 = FolderBrowserDialog()
+                if self.folderBrowserDialog1.ShowDialog() == DialogResult.OK:                    
+                   self.Tboxworkingdir.Text= self.folderBrowserDialog1.SelectedPath                   
+                else:
+                       print "Note there is no folder ...\\inputs\\maps\\ in selected directory!"                 
+        def  Sumup(self,ii):
+           try:
+                    i=str(ii)
+                    if ii==1:
+                         report(self.Tboxworkingdir.Text+"\\"+self.TxtSimulations.Text,"Running at "+DateTime.Now.ToString()+ " ")
+                         self.linData="No"+"\t"+"rain"+"\t"+"wind"+"\t"+"pet"+"\t"+"temp"+"\t"+"gwdepth"+"\t"+"lai"
+                         report(self.Tboxworkingdir.Text+"\\"+self.TxtSimulations.Text,self.linData)
+                         
+                    workingdir(self.Tboxworkingdir.Text+"\\") 
+                    
+                    if(IO.File.Exists(self.Tboxworkingdir.Text+"\\rain\\rain"+i+".asc")== True):
+                        rastermap(self.Tboxworkingdir.Text+"\\rain\\rain"+i+".asc","rain"+i)
+                        X1=cal("MEAN(rain"+i+")")
+                    else:
+                        X1="?"
+                        
+                    if(IO.File.Exists(self.Tboxworkingdir.Text+"\\wind\\wind"+i+".asc")== True):
+                        rastermap(self.Tboxworkingdir.Text+"\\wind\\wind"+i+".asc","wind"+i)
+                        X2=cal("MEAN(wind"+i+")")
+                    else:
+                        X2="?"
+                        
+                    if(IO.File.Exists(self.Tboxworkingdir.Text+"\\pet\\pet"+i+".asc")== True):
+                        rastermap(self.Tboxworkingdir.Text+"\\pet\\pet"+i+".asc","pet"+i)
+                        X3=cal("MEAN(pet"+i+")")
+                    else:
+                        X3="?" 
+                        
+                    if(IO.File.Exists(self.Tboxworkingdir.Text+"\\temp\\temp"+i+".asc")== True):
+                        rastermap(self.Tboxworkingdir.Text+"\\temp\\temp"+i+".asc","temp"+i)
+                        X4=cal("MEAN(temp"+i+")")
+                    else:
+                        X4="?"                        
+                    
+                    if(IO.File.Exists(self.Tboxworkingdir.Text+"\\gwdepth\\gwdepth"+i+".asc")== True):
+                        rastermap(self.Tboxworkingdir.Text+"\\gwdepth\\gwdepth"+i+".asc","gwdepth"+i)
+                        X5=cal("MEAN(gwdepth"+i+")")
+                    else:
+                        X5="?" 
+                        
+                    if(IO.File.Exists(self.Tboxworkingdir.Text+"\\lai\\lai"+i+".asc")== True):
+                        rastermap(self.Tboxworkingdir.Text+"\\lai\\lai"+i+".asc","lai"+i)
+                        X6=cal("MEAN(wind"+i+")")
+                    else:
+                        X5="?"                         
+                    
+                    self.linData=i+"\t"+X1+"\t"+X2+"\t"+X3+"\t"+X4+"\t"+X5
+                    report(self.Tboxworkingdir.Text+"\\"+self.TxtSimulations.Text,self.linData)
+                    
+   
+ 
+           except Exception, exceptionObject:                
+                  MessageBox.Show(exceptionObject.Message.ToString() +'\n' +'Line ' + str(sys.exc_info()[-1].tb_lineno) + '\n'+ "ERROR in aggregation of MAPS!")
+                  raise Exception(exceptionObject.Message.ToString()  )  
+                  
+
+                
+                
+        def loadpage(self):
+                #Define  labelworkindir
+                self.labelworkindir = Label()
+                self.labelworkindir.Font = Drawing.Font("Microsoft Sans Serif", 9, Drawing.FontStyle.Bold, Drawing.GraphicsUnit.Point, 0)
+                self.labelworkindir.Location = Point(8, 4)
+                self.labelworkindir.Name = "labelworkindir"
+                self.labelworkindir.Size = Drawing.Size(250, 23)
+                self.labelworkindir.Text = "WetSpass '~\\inputs\\maps\\'  directory:"            
+                self.Controls.Add(self.labelworkindir)
+                #Define  Tboxworkingdir
+                self.Tboxworkingdir = TextBox()
+                self.Tboxworkingdir.Location = Point(82, 34)
+                self.Tboxworkingdir.Name = "Tboxworkingdir"
+                self.Tboxworkingdir.Size = Drawing.Size(350, 20)
+                self.Tboxworkingdir.Text=""
+                self.Controls.Add(self.Tboxworkingdir)
+                 #< Define  butworkingdir
+                self.butworkingdir = Button()   
+                self.butworkingdir.Location = Point(435, 34)
+                self.butworkingdir.Name = "butworkingdir"
+                self.butworkingdir.Size = Drawing.Size(30, 23)
+                self.butworkingdir.Text = "..."
+                self.butworkingdir.UseVisualStyleBackColor = True
+                self.butworkingdir.Click += self.ButworkdirClick
+                self.Controls.Add(self.butworkingdir) 
+                        
+                #Define the first button
+                self.butDoSomthing = Button()                
+                self.butDoSomthing.BackgroundImageLayout = ImageLayout.None
+                self.butDoSomthing.Location = Point(350, 80)
+                self.butDoSomthing.Name = "butDoSomthing"
+                self.butDoSomthing.Size = Drawing.Size(100, 25)
+                self.butDoSomthing.Text = "Start"
+                self.butDoSomthing.Click += self.DoSomething
+                self.Controls.Add(self.butDoSomthing) 
+                
+                #Define the second button              
+                self.butEditme = Button()                
+                self.butEditme.BackgroundImageLayout = ImageLayout.None
+                self.butEditme.Location = Point(350, 115)
+                self.butEditme.Name = "butEditme"
+                self.butEditme.Size = Drawing.Size(100, 25)
+                self.butEditme.Text = "Edit Me!"
+                self.butEditme.Click += self.EditMyself 
+                self.Controls.Add(self.butEditme) 
+                
+                #< Page object>  labelNoOfTimeSteps
+                self.labelNoOfTimeSteps = Label()
+                self.labelNoOfTimeSteps.Font = Drawing.Font("Microsoft Sans Serif", 9, Drawing.FontStyle.Bold, Drawing.GraphicsUnit.Point, 0)
+                self.labelNoOfTimeSteps.Location = Point(8, 150)
+                self.labelNoOfTimeSteps.Name = "labelNoOfTimeSteps"
+                self.labelNoOfTimeSteps.Size = Drawing.Size(129, 23)
+                self.labelNoOfTimeSteps.Text = "Result file:"
+                self.Controls.Add(self.labelNoOfTimeSteps) 
+                #< Page object>  Tboxnooftimsteps
+                self.TxtSimulations = TextBox()
+                self.TxtSimulations.Location = Point(182, 150)
+                self.TxtSimulations.Name = "Tboxnooftimsteps"
+                self.TxtSimulations.Size = Drawing.Size(80, 20)
+                self.TxtSimulations.Text="Summery.txt"
+                self.Controls.Add(self.TxtSimulations) 
+
+
+                #< Page object>  labelStartTimeSteps
+                self.labelStartTimeSteps = Label()
+                self.labelStartTimeSteps.Font = Drawing.Font("Microsoft Sans Serif", 9, Drawing.FontStyle.Bold, Drawing.GraphicsUnit.Point, 0)
+                self.labelStartTimeSteps.Location = Point(8, 184)
+                self.labelStartTimeSteps.Name = "labelStartTimeSteps"
+                self.labelStartTimeSteps.Size = Drawing.Size(129, 23)
+                self.labelStartTimeSteps.Text = "Starting time step:"
+                self.Controls.Add(self.labelStartTimeSteps) 
+                
+                #< Page object>  Tboxstarttimestep
+                self.Tboxstarttimestep = TextBox()
+                self.Tboxstarttimestep.Location = Point(182, 184)
+                self.Tboxstarttimestep.Name = "Tboxstarttimestep"
+                self.Tboxstarttimestep.Size = Drawing.Size(50, 20)
+                self.Tboxstarttimestep.Text="1"
+                self.Controls.Add(self.Tboxstarttimestep)
+                 
+                
+                #< Page object>  Tboxendtimestep
+                self.Tboxendtimestep = TextBox()
+                self.Tboxendtimestep.Location = Point(182, 218)
+                self.Tboxendtimestep.Name = "Tboxendtimestep"
+                self.Tboxendtimestep.Size = Drawing.Size(50, 20)
+                self.Tboxendtimestep.Text="48"
+                self.Controls.Add(self.Tboxendtimestep)
+                
+                #< Page object>  labelFinalTimeSteps
+                self.labelFinalTimeSteps= Label()
+                self.labelFinalTimeSteps.Font = Drawing.Font("Microsoft Sans Serif", 9, Drawing.FontStyle.Bold, Drawing.GraphicsUnit.Point, 0)
+                self.labelFinalTimeSteps.Location = Point(8, 218)
+                self.labelFinalTimeSteps.Name = "labelFinalTimeSteps"
+                self.labelFinalTimeSteps.Size = Drawing.Size(129, 23)
+                self.labelFinalTimeSteps.Text = "Final time step:"
+                self.Controls.Add(self.labelFinalTimeSteps)
+               #============Form====================== 
+                  
+                # And finally what is MainForm
+
+                self.Size = Size(480, 320)
+                self.allowclose=0
+                self.MinimizeBox = 1
+                self.ShowInTakbar = 0
+                self.FormBorderStyle = FormBorderStyle.Sizable
+                self.GetStayOnTop = 1 
+                rootpath=IO.Path.GetFileNameWithoutExtension( Application.ExecutablePath)     
+                self.Icon= Icon(rootpath+"\\"+rootpath+".ico")
+                self.Name = "Mean"
+                self.Text = "MeanCalc: Summarize input maps. Empowered  by H2PL framework!" 
+               
+MeanCalcPage=MeanCalc.Model().Show()
+print    "Hi, this is the first exprience with H2PL"
+
---
+++ installStack/scripts/makeInstallFiles.py
@@ -0,0 +1,145 @@
+installer = """# Include MUI2 for a better interface
+!include "MUI2.nsh"
+
+!define versionString "{versionString}"
+
+# Configuration for Modern UI
+!define MUI_ABORTWARNING  ; Prompt the user if they try to cancel the installation
+!define MUI_ICON "..\\WetSpass-M\\WetSpass-M\\wetspass-M.ico"
+!define MUI_UNICON "..\\WetSpass-M\\WetSpass-M\\wetspass-M.ico"  ; Uninstaller icon
+!define MUI_BRANDINGTEXT "WetSpass-M Installer"  ; Custom branding text
+
+
+
+BrandingText "WETSPASS-M Installer" 
+
+# Set up the pages
+!insertmacro MUI_PAGE_INSTFILES
+
+# Uninstaller pages (if needed)
+!insertmacro MUI_UNPAGE_INSTFILES
+
+# Uninstaller Title
+!define MUI_UNWINDOWTITLE "WetSpass-M Uninstall"
+
+
+# Define basic information about the installer
+OutFile "Installers\\WetSPASS-v${{versionString}}.exe"
+Name "WetSpass-M"
+InstallDir "C:\\WetSpass-M"
+Icon "..\\WetSpass-M\\WetSpass-M\\wetspass-M.ico"
+RequestExecutionLevel user  ; Does not require admin rights
+!define PRODUCT_NAME "WetSpass-M"
+
+# Define installer properties
+VIProductVersion "${{versionString}}.0"
+VIAddVersionKey /LANG=1033 "ProductName" "WetSpass-M"
+VIAddVersionKey /LANG=1033 "CompanyName" "Sarah Mutua & Celray James"
+VIAddVersionKey /LANG=1033 "FileVersion" "${{versionString}}.0"
+VIAddVersionKey /LANG=1033 "FileDescription" "Installer for WetSpass-M"
+VIAddVersionKey /LANG=1033 "LegalCopyright" "Copyright © Sarah Mutua & Celray James {currentYear}"
+
+Function DotNetMessage
+    MessageBox MB_OK|MB_ICONINFORMATION ".NET 4.0 Runtime is needed to run this software. If you have not installed it, you can install it ster this installation, otherwise, installation will continue. After the finish window, you will find WetSpass-M in Start Menu"
+FunctionEnd
+
+!define UNINSTALLER_KEY "Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\${{PRODUCT_NAME}}"
+
+Function .onInit
+    Call DotNetMessage
+FunctionEnd
+
+# Installer Sections
+Section "Main Files" SEC01
+  SetOutPath "$INSTDIR"  ; Set the output path to the installation directory
+
+  # Add all files from the specified directory
+  File /r "..\\WetSpass-M\\*.*"
+
+  # Create registry entries for Add/Remove Programs
+  WriteRegStr HKCU "${{UNINSTALLER_KEY}}" "DisplayName" "WetSpass-M"
+  WriteRegStr HKCU "${{UNINSTALLER_KEY}}" "UninstallString" "$\\"$INSTDIR\\Uninstall.exe$\\""
+  WriteRegStr HKCU "${{UNINSTALLER_KEY}}" "DisplayIcon" "$INSTDIR\\WetSpass-M\\wetspass-M.ico"
+  WriteRegStr HKCU "${{UNINSTALLER_KEY}}" "DisplayVersion" "${{versionString}}"
+  WriteRegStr HKCU "${{UNINSTALLER_KEY}}" "URLInfoAbout" "https://github.com/celray/WetSpass-M"
+  WriteRegStr HKCU "${{UNINSTALLER_KEY}}" "Publisher" "Sarah Mutua & Celray James"
+  WriteRegDWORD HKCU "${{UNINSTALLER_KEY}}" "NoModify" 1
+  WriteRegDWORD HKCU "${{UNINSTALLER_KEY}}" "NoRepair" 1
+  # Optional: set the estimated size of the installed program (in KB)
+  WriteRegDWORD HKCU "${{UNINSTALLER_KEY}}" "EstimatedSize" 95700
+
+  # Create a shortcut on the desktop and in the start menu
+  CreateDirectory "$DESKTOP"
+  CreateShortCut "$DESKTOP\\WetSpass-M.lnk" "$INSTDIR\\WetSpass-M.exe"
+  CreateDirectory "$SMPROGRAMS\\WetSpass-M"
+  CreateShortCut "$SMPROGRAMS\\WetSpass-M\\WetSpass-M.lnk" "$INSTDIR\\WetSpass-M.exe"
+
+  # Write the uninstaller
+  WriteUninstaller "$INSTDIR\\Uninstall.exe"
+  # ExecShell "open" "$INSTDIR\\WetSpass-M.exe"
+SectionEnd
+
+# Uninstaller Sections
+Section "Uninstall"
+    ; First delete all files in the installation directory
+    RMDir /r "$INSTDIR"
+
+    ; Now attempt to remove the installation directory itself
+    RMDir "$INSTDIR"
+
+    ; Remove shortcuts
+    Delete "$DESKTOP\\WetSpass-M.lnk"
+    Delete "$SMPROGRAMS\\WetSpass-M\\WetSpass-M.lnk"
+    RMDir "$SMPROGRAMS\\WetSpass-M"
+
+    ; Remove registry keys for file association
+    DeleteRegKey HKCR ".spt"
+    DeleteRegKey HKCR "WetSpass.Project"
+
+    ; Remove the uninstaller itself
+    Delete "$INSTDIR\\Uninstall.exe"
+
+    # Remove registry keys
+    DeleteRegKey HKCU "${{UNINSTALLER_KEY}}"
+
+SectionEnd
+"""
+
+
+import sys, os, shutil
+import subprocess
+from genericpath import exists
+from cjfx import write_to, delete_file
+import datetime
+
+if len(sys.argv) < 2:
+    print("Error: Please provide the version number")
+    sys.exit()
+
+version = sys.argv[1]
+
+# function to run nsi script
+
+# one level up from file directory
+os.chdir(os.path.dirname(__file__))
+os.chdir("../")
+
+
+def runNSIScript(scriptPath, nsisPath):
+    if not scriptPath.endswith('.nsi'):
+        print("Error: scriptPath should be a .nsi file")
+        return
+    subprocess.run(f'"{nsisPath}" "{scriptPath}"', shell=True)
+
+mainInstallerScript = os.path.join(os.path.dirname(__file__), "../tmp1.nsi")
+
+write_to(mainInstallerScript, installer.format(versionString=version, currentYear=datetime.datetime.now().year))
+
+# create folder 'Installers' if it does not exis
+if not os.path.exists("Installers"): os.makedirs("Installers")
+makensisPath = "C:/Program Files (x86)/NSIS/Bin/makensis.exe"
+
+runNSIScript(mainInstallerScript, makensisPath)
+
+delete_file(mainInstallerScript)
+print("done!")
